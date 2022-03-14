30f8639d9f528393b18774adbb0a5db29909c829
Alexander Voronov <dr.strum@gmail.com>
2022-03-14 14:03:30 +0300

    Made ViewModel and Observer

--- app/src/main/java/ru/gb/course2/gblesson2/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/gb/course2/gblesson2/ui/main/MainViewModel.kt
@@ -1,7 +1,9 @@
 package ru.gb.course2.gblesson2.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val liveDataToObserve: MutableLiveData<Any> = MutableLiveData()) :
+    ViewModel() {
+    fun getLiveData() = liveDataToObserve
 }
