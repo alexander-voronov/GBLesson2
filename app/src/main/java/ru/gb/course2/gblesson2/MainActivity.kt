55c2ed8aed3453ca99b514d7e12311e0d4d207ae
Alexander Voronov <dr.strum@gmail.com>
2022-03-14 12:20:35 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/course2/gblesson2/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.gb.course2.gblesson2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.gb.course2.gblesson2.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/gb/course2/gblesson2/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.gb.course2.gblesson2.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
