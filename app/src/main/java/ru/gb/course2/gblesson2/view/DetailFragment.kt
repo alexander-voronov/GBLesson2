6c705023dc2281a7437d3c87395974859b9465f8
Alexander Voronov <dr.strum@gmail.com>
2022-04-25 17:48:16 +0300

    Refactored DetailFragment.kt

--- app/src/main/java/ru/gb/course2/gblesson2/view/DetailFragment.kt
+++ app/src/main/java/ru/gb/course2/gblesson2/view/DetailFragment.kt
@@ -1,17 +1,33 @@
 package ru.gb.course2.gblesson2.view
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
 import ru.gb.course2.gblesson2.R
 import ru.gb.course2.gblesson2.data.Weather
+import ru.gb.course2.gblesson2.data.WeatherDTO
 import ru.gb.course2.gblesson2.databinding.FragmentDetailBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
+
+private const val YOUR_API_KEY = "aab7df1b-2d95-4202-af43-6aad806bee38"
 
 class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
+    private lateinit var weatherBundle: Weather
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,20 +37,77 @@ class DetailFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?.let { weather ->
-            with(binding) {
-                cityName.text = weather.city.city
-                cityCoordinates.text = String.format(
-                    getString(R.string.city_coordinates),
-                    weather.city.lat.toString(),
-                    weather.city.lon.toString()
-                )
-                temperatureValue.text = weather.temperature.toString()
-                feelsLikeValue.text = weather.feelsLike.toString()
-            }
+            weatherBundle = weather
         }
+        binding.mainView.visibility = View.GONE
+        binding.loadingLayout.visibility = View.VISIBLE
+        loadWeather()
+    }
+
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding) {
+            mainView.visibility = View.VISIBLE
+            loadingLayout.visibility = View.GONE
+            val city = weatherBundle.city
+            cityName.text = city.city
+            cityCoordinates.text = String.format(
+                getString(R.string.city_coordinates),
+                city.lat.toString(),
+                city.lon.toString()
+            )
+            weatherCondition.text = weatherDTO.fact?.condition
+            temperatureValue.text = weatherDTO.fact?.temp.toString()
+            feelsLikeValue.text = weatherDTO.fact?.feels_like.toString()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun loadWeather() {
+        try {
+            val uri =
+                URL("https://api.weather.yandex.ru/v2/informers?lat=${weatherBundle.city.lat}&lon=${weatherBundle.city.lon}")
+            val handler = Handler()
+            Thread(
+                Runnable {
+                    lateinit var urlConnection: HttpsURLConnection
+                    try {
+                        urlConnection = uri.openConnection() as HttpsURLConnection
+                        urlConnection.requestMethod = "GET"
+                        urlConnection.addRequestProperty(
+                            "X-Yandex-API-Key",
+                            YOUR_API_KEY
+                        )
+                        urlConnection.readTimeout = 10000
+                        val bufferedReader =
+                            BufferedReader(InputStreamReader(urlConnection.inputStream))
+// преобразование ответа от сервера (JSON) в модель данных (WeatherDTO)
+                        val weatherDTO: WeatherDTO =
+                            Gson().fromJson(
+                                getLines(bufferedReader),
+                                WeatherDTO::class.java
+                            )
+                        handler.post { displayWeather(weatherDTO) }
+                    } catch (e: Exception) {
+                        Log.e("", "Fail connection", e)
+                        e.printStackTrace()
+                    } finally {
+                        urlConnection.disconnect()
+                    }
+                }
+            ).start()
+        } catch (e: MalformedURLException) {
+            Log.e("", "Fail URI", e)
+            e.printStackTrace()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getLines(reader: BufferedReader): String {
+        return reader.lines().collect(Collectors.joining("\n"))
     }
 
     companion object {
