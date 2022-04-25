2b57e4f99540635b005762b8b082b209ea8142f8
Alexander Voronov <dr.strum@gmail.com>
2022-04-25 13:24:25 +0300

    Added data classes and fixed fragment_detail.xml

---
+++ app/src/main/java/ru/gb/course2/gblesson2/data/FactDTO.kt
@@ -0,0 +1,7 @@
+package ru.gb.course2.gblesson2.data
+
+data class FactDTO(
+    val temp: Int?,
+    val feels_like: Int?,
+    val condition: String?
+)
---
+++ app/src/main/java/ru/gb/course2/gblesson2/data/WeatherDTO.kt
@@ -0,0 +1,5 @@
+package ru.gb.course2.gblesson2.data
+
+data class WeatherDTO(
+    val fact: FactDTO?
+)
